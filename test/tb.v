dd6391b0462d6a6c333bdbe4a92e59aaddb4bafb
cherny1001 <cherny@email.cz>
2026-06-22 21:33:21 +0200

    fixed  tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_cherny_xor_8bi (
+  
+  tt_um_cherny_xor_8bi user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
